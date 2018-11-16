c387c763afa4dcd0f9f2a724d6c4bb0fdc4a2b8d
julieh <[julie.huang@alumni.ubc.ca]>
2018-11-15 23:17:39 -0800

    added all tables

--- packages/api/database/migration/20181111142337_create_hotel_db.js
+++ packages/api/database/migration/20181111142337_create_hotel_db.js
@@ -27,7 +27,58 @@ exports.up = async function(knex, Promise) {
       knex.schema.raw('CREATE TABLE "account" ' +
           '("username" varchar(31) PRIMARY KEY not null, ' +
           '"user_id" uuid REFERENCES users(id), ' +
-          '"password" varchar(31) not null )')
+          '"password" varchar(31) not null )'),
+
+      knex.schema.raw('CREATE TABLE "room" ' +
+          '("room_id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(), ' +
+          '"price" float(53), ' +
+          '"num_beds" integer, ' +
+          '"max_guests" integer, ' +
+          '"bedType" varchar(31), ' +
+          '"availability" bool default true, ' +
+          '"pets_allowed" bool default false )'),
+
+      knex.schema.raw('CREATE TABLE "amenity" ' +
+          '("amenity_id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(), ' +
+          '"opening" varchar(31), ' +
+          '"closing" varchar(31), ' +
+          '"rating" int, ' +
+          '"type" varchar(31) ) '),
+
+      knex.schema.raw('CREATE TABLE "restaurant" ' +
+          '("restaurant_id" uuid PRIMARY KEY REFERENCES amenity(amenity_id), ' +
+          '"hasBar" bool default false, ' +
+          '"cuisineType" varchar(31), ' +
+          '"budget" int )' ),
+
+      knex.schema.raw('CREATE TABLE "spa" ' +
+          '("spa_id" uuid PRIMARY KEY REFERENCES amenity(amenity_id), ' +
+          '"hasMassage" bool default false, ' +
+          '"hasSalon" bool default false, ' +
+          '"hasHotTub" bool default false, ' +
+          '"hasTanBed" bool default false )' ),
+
+      knex.schema.raw('CREATE TABLE "gym" ' +
+          '("gym_id" uuid PRIMARY KEY REFERENCES amenity(amenity_id), ' +
+          '"hasWeights" bool default false, ' +
+          '"hasPool" bool default false, ' +
+          '"hasClass" bool default false, ' +
+          '"hasCardio" bool default false )' ),
+
+      //Reservation(reservation.id:Char,  user.id:Char, room.id:Char, hotel.id:Char,
+      // isCheckedIn:Bool, isCheckedOut:Bool, has.breakfast:Bool, num.guests:Integer, checkOutDate:Char, checkInDate:char)
+      knex.schema.raw('CREATE TABLE "reservation" ' +
+          '("res_id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(), ' +
+          '"user" uuid REFERENCES users(id), ' +
+          '"room" uuid REFERENCES room(room_id), ' +
+          '"hotel" uuid REFERENCES hotel(hotel_id), ' +
+          '"isCheckedIn" bool default false, ' +
+          '"isCheckedOut" bool default false, ' +
+          '"has.breakfast" bool default false, ' +
+          '"num.guests" int, ' +
+          '"checkOutDate" varchar(31) not null, ' +
+          '"checkInDate" varchar(31) not null )' )
+
 
     ])
 
@@ -36,7 +87,12 @@ exports.up = async function(knex, Promise) {
 exports.down = function(knex, Promise) {
   //remove all the tables
   return Promise.all([
-
+      knex.schema.dropTable('reservation'),
+      knex.schema.dropTable('gym'),
+      knex.schema.dropTable('spa'),
+      knex.schema.dropTable('restaurant'),
+      knex.schema.dropTable('amenity'),
+      knex.schema.dropTable('room'),
       knex.schema.dropTable('employees'),
       knex.schema.dropTable('account'),
       knex.schema.dropTable('hotel'),
